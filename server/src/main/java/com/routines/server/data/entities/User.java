7587482dd832adc0d8f33ca5c43fe23d39c483a8
Jason Beisiegel <jasonbeisiegel@gmail.com>
2026-06-26 21:09:10 -0400

    adds default constructor to User entity

--- server/src/main/java/com/routines/server/data/entities/User.java
+++ server/src/main/java/com/routines/server/data/entities/User.java
@@ -32,6 +32,8 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Task> tasks;
 
+    public User() {}
+
     public User(String email, String firstName, String lastName) {
         this.email = email;
         this.firstName = firstName;
